4b1f1c235c462ceba717a3fb5bd392ab9b166045
Carolina Amorim <carolinaoa1@al.insper.edu.com>
2025-10-07 08:29:00 -0300

    handout - Implementando a Criação de Anotações

--- notes-frontend/src/App.jsx
+++ notes-frontend/src/App.jsx
@@ -2,21 +2,27 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Note from "./components/Note";
 import AppBar from "./components/AppBar";
+import Formulario from "./components/Formulario";
 import "./App.css";
 
 function App() {
   const [notes, setNotes] = useState([]);
 
-  useEffect(() => {
+  const carregaNotas = () => {
     axios
       .get("http://localhost:8000/api/notes/")
       .then((res) => setNotes(res.data));
+  }
+
+  useEffect(() => {
+    carregaNotas();
   }, []);
 
   return (
     <>
       <AppBar />
       <main className="container">
+        <Formulario loadNotes={carregaNotas}/>
         <div className="card-container">
           {notes.map((note) => (
             <Note key={`note__${note.id}`} title={note.title}>
