137b7e2be1fc99aa730068fb5dadcb6838aadc7a
Carolina Amorim <carolinaoa1@al.insper.edu.com>
2025-10-07 08:04:22 -0300

    handout - Componentes

--- notes-frontend/src/App.jsx
+++ notes-frontend/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Note from "./components/Note";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const notes = [
+    {
+      id: 1,
+      title: "Receita de miojo",
+      content:
+        "Bata com um martelo antes de abrir o pacote. Misture o tempero, coloque em uma vasilha e aproveite seu snack :)",
+    },
+    {
+      id: 2,
+      title: "Sorvete de banana",
+      content: "Coloque a banana no congelador e espere.",
+    },
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {notes.map((note) => (
+        <Note key={`note__${note.id}`} title={note.title}>
+          {note.content}
+        </Note>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
